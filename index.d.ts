60effc7349371ef72ab8ab931dd0f8d9daca4d5d
Douglas Nassif Roma Junior <douglasjunior@users.noreply.github.com>
2020-12-22 10:06:52 -0300

    Fix export default type definition

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,36 @@
-declare module "react-native-get-location" {
-  export class Location {
-    latitude: number;
-    longitude: number;
-    altitude: number;
-    accuracy: number;
-    speed: any;
-    time: number;
-    bearing?: number;
-    provider?: number;
-    verticalAccuracy?: number;
-    course?: number;
-  }
-  export interface GetCurrentPositionOptions {
-    enableHighAccuracy: boolean;
-    timeout: number;
-  }
 
-  export type LocationErrorCode =
-    | "CANCELLED"
-    | "UNAVAILABLE"
-    | "TIMEOUT"
-    | "UNAUTHORIZED";
+export class Location {
+  latitude: number;
+  longitude: number;
+  altitude: number;
+  accuracy: number;
+  speed: any;
+  time: number;
+  bearing?: number;
+  provider?: number;
+  verticalAccuracy?: number;
+  course?: number;
+}
 
-  export class LocationError extends Error {
-    code: LocationErrorCode;
-  }
+export interface GetCurrentPositionOptions {
+  enableHighAccuracy: boolean;
+  timeout: number;
+}
 
-  export function getCurrentPosition(
-    options: GetCurrentPositionOptions
-  ): Promise<Location>;
+export type LocationErrorCode =
+  | "CANCELLED"
+  | "UNAVAILABLE"
+  | "TIMEOUT"
+  | "UNAUTHORIZED";
+
+export class LocationError extends Error {
+  code: LocationErrorCode;
+}
+
+function getCurrentPosition(
+  options: GetCurrentPositionOptions
+): Promise<Location>;
+
+export default {
+  getCurrentPosition
 }
